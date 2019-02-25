9ac5ad214e5451e59b2a0ae76ef565abff083ce8
Danny Martin <danielmartin0126@gmail.com>
2019-02-25 09:08:36 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,28 @@
 def unsafe?(speed)
 	if speed > 60
+<<<<<<< HEAD
 		return true
 	elsif speed < 40
 		return true
 	else
 		return false
 end
+=======
+		true
+	elsif speed < 40
+		true
+	else
+		false
+
+>>>>>>> 75897b47cfc8590d2853f01c519cb119ddd13351
 end
 
 
 
 def not_safe?(speed)
 (speed > 60 || speed < 40) ? true : false
+<<<<<<< HEAD
+=======
+end
+>>>>>>> 75897b47cfc8590d2853f01c519cb119ddd13351
 end
